18341820d7f45f79d6fc97f51531bc4dfd20caaf
uday.kondaveeti <uday.kondaveeti@aa.com>
2019-10-24 10:13:22 -0500

    functional interface demo added

---
+++ src/main/java/com/learning/functionalinterface/FIDemo.java
@@ -0,0 +1,11 @@
+package com.learning.functionalinterface;
+
+/**
+ * Lambdas represent implementation of a functional interface.
+ * Functional interface has only one abstract method
+ */
+@FunctionalInterface
+public interface FIDemo {
+
+    public abstract void greet(String name);
+}
---
+++ src/main/java/com/learning/functionalinterface/FIMain.java
@@ -0,0 +1,30 @@
+package com.learning.functionalinterface;
+
+public class FIMain {
+
+    public static void main(String [] args){
+
+        FIDemo fid = new FIDemo() {
+            @Override
+            public void greet(String name) {
+                System.out.println("Hello" + name);
+            }
+        };
+
+        fid.greet("uday");
+
+        //Lambdas makes implement above code easily
+        //with variable , variable type, no args
+
+        FIDemo fid1 = (String name)  -> {
+            System.out.println("Hello" + name);
+        };
+
+        fid1.greet("uday");
+        FIDemo fid2 = (name)  -> {
+            System.out.println("Hello" + name);
+        };
+
+        fid2.greet("uday");
+    }
+}
